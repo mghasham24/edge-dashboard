d36be7fdcfdb74b1e4d0bac6f2e47d83dd66e706
mghasham24 <you@example.com>
2026-03-22 00:46:10 -0500

    Add consensus odds from 4 bookmakers

--- netlify/functions/odds.js
+++ netlify/functions/odds.js
@@ -5,10 +5,11 @@ exports.handler = async function(event) {
   }
 
   const params = event.queryStringParameters || {};
-  const sport  = params.sport  || 'basketball_nba';
-  const markets = params.markets || 'h2h';
+  const sport      = params.sport      || 'basketball_nba';
+  const markets    = params.markets    || 'h2h';
+  const bookmakers = params.bookmakers || 'fanduel,draftkings,betmgm,caesars';
 
-  const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us&markets=${markets}&bookmakers=fanduel&oddsFormat=american`;
+  const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us&markets=${markets}&bookmakers=${bookmakers}&oddsFormat=american`;
 
   try {
     const res  = await fetch(url);
